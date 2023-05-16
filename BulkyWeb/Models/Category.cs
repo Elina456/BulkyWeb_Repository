5dcb415488ad561337bc53464b3bedfa26778a5c
Elina456 <ealinagupta2074@gmail.com>
2023-05-16 16:04:17 +0530

    Category CRUD Operations

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+	public class Category
+	{
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("CATEGORY NAME")]
+        public string Name{ get; set; }
+
+        [DisplayName("DISPLAY ORDER")]
+        [Range(1,100,ErrorMessage ="DISPLAY ORDER MUST BE BETWEEN 1-100")]
+        public int DisplayOrder { get; set; }
+
+
+    }
+}
